01c9a6a96513e6b24e5b63a46a26908fbfaa85f1
kaptan000 <kaptan.singh@hcode.tech>
2023-11-07 18:08:42 +0530

    conditions added

---
+++ python1/conditions.py
@@ -0,0 +1,15 @@
+x = 2
+print(x==2)
+print(x==3)
+name = 'john'
+age =23
+if name == 'john' and age == 23:
+    print('true')
+if name == 'john' or name == 'rick':
+    print('true')    
+    
+x = 2
+if x==2:
+    print('x exuals 2!')    
+y = 2
+print(x is y)    
