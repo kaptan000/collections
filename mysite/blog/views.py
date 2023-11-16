54300b7724ba62c45b77b67f3804b89238c95005
kaptan000 <kaptan.singh@hcode.tech>
2023-11-16 10:27:30 +0530

    Building blog - Data & models

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,29 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from datetime import date
+from .models import Post
 # Create your views here.
-all_posts = [
-    {
-        "slug":"hike-in-the-mountains",
-        "image":"mountains.jpg",
-        "author":"Maximilian",
-        "date":date(2021,7,21),
-        "title":"Mountain Hiking",
-        "excerpt":"There is nothing like the views you get when hiking in mountains.",
-        "content":"lorem cdsjc cdsnjv xdvj dskj cdsojcdcdscdfv dsvf cfdln cd qw  kmnb"
-    }
-]
+
 
 def get_date(post):
     return post['date']
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by('-date')[:3]
     return render(request,"blog/index.html" , {"posts": latest_posts})
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request,"blog/all-posts.html",{"all_posts":all_posts})
 
 def post_detail(request,slug):
-    identified_post = next(post for post in all_posts if post['slug']==slug)
-    return render(request,"blog/post-detail.html",{"post":identified_post})
+    identified_post = get_object_or_404(Post,slug=slug)
+    return render(request,"blog/post-detail.html",{"post":identified_post,"post_tags":identified_post.tag.all()})
